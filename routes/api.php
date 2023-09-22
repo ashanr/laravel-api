0a74f244966f14da8d244d36e131142bbf0f3802
Ashan Rajapaksha <rajapaksha.live@gmail.com>
2023-09-23 02:48:06 +0530

    routes api updated

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use app/Http/Controllers/UserController;
+// use app/Http/Controllers/UserController;
 
 /*
 |--------------------------------------------------------------------------
